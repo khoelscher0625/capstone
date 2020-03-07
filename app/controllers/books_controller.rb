417407cb04220e46473531cd3847580e2752c09f
Kathryn Hoelscher <khoelscher0625@gmail.com>
2020-03-07 17:05:38 +0000

    fixed bugs with new page, added admin confirmation logic

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,21 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!
-
+  before_action :authenticate_user! 
+  before_action :confirm_admin_status, only: [:new]
+  
   def index
     @books = Book.paginate(page: params[:page], per_page: 20) 
   end
 
-  def show
-    @books = Book.all
-  end
-
   def new
-    @books = Book.new
+    @book = Book.new
   end
 
+  private
+
+  def confirm_admin_status
+    if current_user.admin == false
+      redirect_to root_page
+    end
+  end
 end
 
--- app/models/book_name.rb
+++
@@ -1,3 +0,0 @@
-class BookName < ApplicationRecord
-  
-end
