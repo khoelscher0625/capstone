73baa40b32f0ba12cc497909548c91d5e82691bc
Kathryn Hoelscher <khoelscher0625@gmail.com>
2020-03-09 23:31:04 +0000

    changed admin status for me to be able to view new page, added additional CRUD actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,62 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = current_user.book.create(book_params)
+      if @book.valid?
+        redirect_to root_path
+      else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+
+    if @book.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+  end
+
+  def update
+     @book = Book.find(params[:id])
+    if @book.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
+    @book.update_attributes(place_params)
+    if @book.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+   def destroy
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
+    @book.destroy
+    redirect_to root_path
+  end
+
   private
 
   def confirm_admin_status
     if current_user.admin == false
-      redirect_to root_page
+      redirect_to root_path
     end
   end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description, :level)
+  end
+
 end
 
